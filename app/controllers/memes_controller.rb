5413ef78d3de5832dc077fe1de249022a98b5122
Alexandre Coulmy <alexandre.coulmy@gmail.com>
2020-10-04 15:45:29 +0200

    migration ccahe votabme

--- app/controllers/memes_controller.rb
+++ app/controllers/memes_controller.rb
@@ -1,9 +1,22 @@
 class MemesController < ApplicationController
+  before_action :authenticate_user!, only: [:vote]
+  respond_to :js, :json, :html
+
   def index
     @memes = Meme.all
+    @meme = Meme.find(1)
   end
 
   def new
     @meme = Meme.new
+  end
+
+  def vote
+  end
+
+  def meme_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:meme).permit(:title, :source)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'about', to: 'pages#about'
-  get 'memestories', to: 'memes#memestories'
+
+  resources :memes do
+    puts "like" => "memes#vote"
+  end
 end
